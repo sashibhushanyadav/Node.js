72193ff1d47c953590cab3bc81f8ed0f9c5bde83
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-12-21 22:48:27 +0545

    feat:module export.

---
+++ day2/module Export/index.js
@@ -0,0 +1,7 @@
+console.log("hello Sashi");
+const add = require("./sumFunction");
+
+const sum = add(5, 3);
+const sum2 = add(9, 3);
+console.log(sum);
+console.log(sum2);
---
+++ day2/module Export/sumFunction.js
@@ -0,0 +1,5 @@
+const num = (a, b) => {
+  return a + b;
+};
+
+module.exports = num;
