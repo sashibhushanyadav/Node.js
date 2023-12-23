c5c0f21ab131d41cb465a164f4ea206942feeaa7
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-12-23 22:19:07 +0545

    feat: wrapper module.

---
+++ day2/module Wrapper/iife.js
@@ -0,0 +1,16 @@
+
+// NOTE: function contains five parameters 
+// Which is exports, require, module, _ _filename, _ _dirname
+
+
+
+
+(function (message) {
+  const a = "first";
+  console.log(message, a);
+})("hello");
+
+(function (message) {
+  const a = "second";
+  console.log(message, a);
+})("sashi");
