d33c89e3454120184f39e80449699bbb68108051
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-12-28 20:46:59 +0545

    feat: note about module.exports vs exports.

---
+++ day2/module.exports vs export/index.js
@@ -0,0 +1,11 @@
+// Note
+
+// EXPORTS
+// -while using only exports instead of module.export 
+// we get only data in exports parameters which is 
+// outside the module and inside the local
+
+// MODULE.EXPORTS
+// -while using module.exports instead of exports, 
+// It shows the data inside the exports which is 
+// inside the module parameters.
