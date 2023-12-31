d4e4e360fe9505585895e176d6917884192ec202
sashibhushanyadav <contact.sashibhushan@gmail.com>
2023-12-31 21:56:52 +0545

    feat: json module and watch mode.

---
+++ json/index.js
@@ -0,0 +1,4 @@
+const data = require("./data.json");
+// const data = require("./data");
+// this is also work even we removed .json extension
+console.log(data.name);
---
+++ json/watchMode.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+
+// Watch Mode 
+// node --watch followed by fileName
