172a0c4ed1b3afc1e213c28598631ed414b77967
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-01-07 21:45:20 +0545

    feat:complete event modules.

--- events Module-D12/index.js
+++ events Module-D12/index.js
@@ -1,4 +1,17 @@
-const eventModule = require("eventModule");
+const events = require("node:events");
+// console.log("sassshi");
+const emitter = new events();
 
-console.log(eventModule("sassshi"));
- 
+emitter.on("order-pizza", (size, topping) => {
+  console.log(`Order received! Baking a ${size} pizza with ${topping}`);
+});
+
+emitter.on("order-pizza", (size) => {
+  if (size === "large") {
+    console.log("Serving complimentary drink");
+  }
+});
+
+console.log("Do work before event occurs in the system");
+
+emitter.emit("order-pizza", "large", "mushrooms");
