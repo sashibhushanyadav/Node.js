2da7c82566a1c3acdd717d15e04116ea7ef8a255
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-01-21 18:49:15 +0545

    feat:just the definition of character sets and encoding

---
+++ Character-D14/index.js
@@ -0,0 +1,4 @@
+
+// Computer only understand binary data which is 0 and 1.
+// The process of converting any predefined characters (audio,vedio,letter etc) into numbers is called characters set.
+// The process of converting any numbers or character sets into binary data, which is called character encoding. 
