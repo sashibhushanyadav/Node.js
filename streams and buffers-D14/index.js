663da62f67e2c751b4f0ed3e2af6323d82414d95
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-01-27 20:03:03 +0545

    feat:write definition of streams and chunk

--- streams and buffers-D14/index.js
+++ streams and buffers-D14/index.js
@@ -1,4 +1,8 @@
 const buffer = new Buffer.from("Bhushan");
+// NOTE
+// Stream is a sequence of data which moves from one point to another with in a time.
+//Chunk is a small piece of data
+// Chunking is the process of splitting data into a smaller one (The smaller one is called chunk)
 
 buffer.write("sashi");
 buffer.write("sashiyadav");
