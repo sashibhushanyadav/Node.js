2580440f0affc482d39f624400a0b1e6a7fec1c0
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-01-30 23:40:37 +0545

    feat:concept of HTTP

---
+++ http module-D20/index.js
@@ -0,0 +1,11 @@
+
+
+// if computer is conneceted to the internet then we called it, clients and servers
+// Clients: it is a internet-connected devices such as mobile phones or computers
+// Servers:it is a computers that store web pages, sites, or apps
+
+// Client Server Model:
+// when you type a URL in the browser the client device requests access to a web page and a copy of the web page is downloaded from the server and sent as a response to the client to be displayed in the web browser.
+
+// HTTP-hpertext transfer protocol
+// A protocol that defines a format for clients and servers to speak to each other
