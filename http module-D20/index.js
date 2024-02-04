b4a25e77b11f8d70d52d4fd122d690f03a5eb9f3
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-04 23:18:42 +0545

    fix: definition of client sever model.

--- http module-D20/index.js
+++ http module-D20/index.js
@@ -5,7 +5,9 @@
 // Servers:it is a computers that store web pages, sites, or apps
 
 // Client Server Model:
-// when you type a URL in the browser the client device requests access to a web page and a copy of the web page is downloaded from the server and sent as a response to the client to be displayed in the web browser.
+// (when you type a URL in the browser the client device requests access to a web page 
+// and a copy of the web page is downloaded from the server and sent as a response to the 
+// client to be displayed in the web browser.)
 
 // HTTP-hpertext transfer protocol
 // A protocol that defines a format for clients and servers to speak to each other
