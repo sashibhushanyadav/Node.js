682d7da574bbe35cbbbad6ecb460df53c0f17831
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-05 19:34:24 +0545

    feat: definition of libuv

---
+++ D28-libuv/index.js
@@ -0,0 +1,9 @@
+// What?
+// libuv is a cross platform open source library written in C language
+
+// Why?
+// handles asynchronous non-blocking operations in Node.js
+
+// How?
+// Thread pool 
+// Event loop
