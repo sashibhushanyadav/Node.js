34cecd653bf6812876a2c5cddca03091973deb5f
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-05 19:33:54 +0545

    feat: node.js runtime

---
+++ D27-Node Runtime/index.js
@@ -0,0 +1,9 @@
+// Node.js Runtime
+
+// Definition: (It is an environment which provides all the necessary 
+// components in order to use and run a javascript program).
+
+// It contains three major components
+// external dependencies: like V8 and libuv which are libraries
+// c/c++ features: like file system access and networking
+// js library: which provides functions and utilities.
