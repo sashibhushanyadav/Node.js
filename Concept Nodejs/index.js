9188340fcfc4120a50e591c4c7818eba954877d4
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-10 22:21:09 +0545

    feat: basic concept of node js

---
+++ Concept Nodejs/index.js
@@ -0,0 +1,24 @@
+// // Node.js
+// It is an open-source (public available for sharing and modification), cross-platform (available on Mac, Windows and Linux), JavaScript runtime environment.
+
+// // JAVASCRIPT CODE
+// JavaScript code cannot understand by the computer
+
+// // JAVASCRIPT ENGINE
+// JavaScript engine is a program that converts JavaScript code to understand the computer to perform specific tasks.JavaScript.JavaScript.
+// (simply JavaScript engine can execute JavaScript code)
+
+// // JAVASCRIPT ENGINE
+// i. V8-for chrome
+// ii.SpiderMonkey-for Mozilla Firefox
+// iii.JavaScriptCore-for safari
+// iv.Chakra- for microsoft edge
+
+// // JAVASCRIPT RUNTIME
+// JavaScript runtime is an environment which provides all the necessary components in order to use and run a JavaScript engine.
+// The components of JavaScript runtime are 
+// -js engine(like V8 which contains memory heap & call stack), 
+// -web APIs (DOM, Storage, Timers), 
+// -Event loop (CallBack/Task Queue, Microtask Queue)
+
+// // 
