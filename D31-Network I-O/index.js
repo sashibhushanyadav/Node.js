fae5b8c5a6e517750ff086f487cbc18a8080f28d
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-11 11:24:39 +0545

    fix: the definition.

--- D31-Network I-O/index.js
+++ D31-Network I-O/index.js
@@ -1,13 +1,14 @@
 const https = require("node:https");
 
-// In node.js async methods are handle by libuv.
-// They are handled in two different ways(i. Native async mechanism ii. Thread pool)
-// whenever possible, Libuv will use native async mechanism in the OS(operating system) so as to avoid blocking the main thread.
+// -In node.js async methods are handle by libuv.
+// -They are handled in two different ways(i. Native async mechanism ii. Thread pool).
+// -whenever possible, Libuv will use native async mechanism in the OS(operating system) 
+//   so as to avoid blocking the main thread.
 
 // NOTE: 
-// -https.request is a network input/output operation and not a CPU bound operation.
+// -https.request is a network I/O (input/output) operation and not a CPU bound operation.
 // -(although both crypto.pbkdf2 and https.request are asynchronous,
-// https.request method does not seem to use the thread pool).
+//   https.request method does not seem to use the thread pool).
 // -https.request does not seem to be affected by the number of CPU cores either.
 
 const MAX_CALLS = 10;
