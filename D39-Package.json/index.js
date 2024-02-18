5cc03fcdc8940c4430285447c3ab7d9f1b252d7a
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-18 23:04:35 +0545

    feat: complete package.json

---
+++ D39-Package.json/index.js
@@ -0,0 +1,8 @@
+function hello(name, place) {
+  console.log(`My name is ${name} Yadav. I live at ${place}`);
+}
+
+module.exports = hello;
+
+// Use "npm init"
+// In the case of default we can use "npm init --yes"
