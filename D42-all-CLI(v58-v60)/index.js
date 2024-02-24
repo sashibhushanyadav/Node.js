2a6a1ce9adee42be19f110a2c02ac0a95f78b407
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-24 21:31:29 +0545

    feat: CLI option

--- D42-all-CLI(v58-v60)/index.js
+++ D42-all-CLI(v58-v60)/index.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 
-console.log("hello Sashi");
+// NOTE:
+// "/usr/local/bin/node" - zero index path to the interpreter
+// "/usr/local/bin/sashi-clipokedox" - first index path to the CLI
+// "--pokemon=charmander" - second index passed in option
+
+const yargs = require("yargs");
+const { argv } = yargs(process.argv);
+
+const printFiveNames = async (pokemonName) => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  );
+  const value = await response.json();
+  //   console.log(value.moves);
+  const fiveMove = value.moves.map(({ move }) => {
+    // console.log(names.move.name);
+    return move.name;
+  });
+  console.log(fiveMove.slice(0, 5));
+};
+printFiveNames(argv.pokemon);
