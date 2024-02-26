6ce45ec90ac15992fc888a07bf3fcb9a8b487cc0
sashibhushanyadav <contact.sashibhushan@gmail.com>
2024-02-26 12:20:33 +0545

    feat: interactive CLI tool

--- D43-Interactive CLI tools/index.js
+++ D43-Interactive CLI tools/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
-const {argv} = yargs(process.argv);
+const inquirer = require("inquirer");
 
 const apiProduct = async (productName) => {
   const response = await fetch(
@@ -13,4 +12,15 @@ const apiProduct = async (productName) => {
   });
   console.log(data.slice(0, 5));
 };
-apiProduct(argv.pokemon);
+
+const prompt = inquirer.createPromptModule();
+prompt([
+  {
+    type: "input",
+    name: "pokemon",
+    message: "Enter a pokemon name to view its first 5 moves",
+  },
+]).then((answer) => {
+  const pokemon = answer.pokemon;
+  apiProduct(pokemon);
+});
